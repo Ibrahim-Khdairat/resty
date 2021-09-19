e577c330b97cb0f7e69d255d6a49d7d03f5d3dcf
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-09-19 17:22:46 +0300

    solving lab 26

---
+++ src/components/results/index.js
@@ -0,0 +1,12 @@
+
+function Results () {
+  
+    return (
+      <section>
+        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+      </section>
+    );
+  }
+
+
+export default Results;
